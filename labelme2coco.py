b5db7fea1bec39b4839b75fe700ce149e7797e61
zhoujianwen <zhou_jianwen@qq.com>
2020-10-28 18:22:45 +0800

    zhou_jianwen@qq.com
    git push all

---
+++ labelme2coco.py
@@ -0,0 +1,136 @@
+import os
+import json
+import numpy as np
+import glob
+import shutil
+from sklearn.model_selection import train_test_split
+np.random.seed(41)
+
+#0为背景
+classname_to_id = {"short": 1,"solder":2,"solderball":3}
+
+class Lableme2CoCo:
+
+    def __init__(self):
+        self.images = []
+        self.annotations = []
+        self.categories = []
+        self.img_id = 0
+        self.ann_id = 0
+
+    def save_coco_json(self, instance, save_path):
+        json.dump(instance, open(save_path, 'w', encoding='utf-8'), ensure_ascii=False, indent=1)  # indent=2 更加美观显示
+
+    # 由json文件构建COCO
+    def to_coco(self, json_path_list):
+        self._init_categories()
+        for json_path in json_path_list:
+            obj = self.read_jsonfile(json_path)
+            self.images.append(self._image(obj, json_path))
+            shapes = obj['shapes']
+            for shape in shapes:
+                annotation = self._annotation(shape)
+                self.annotations.append(annotation)
+                self.ann_id += 1
+            self.img_id += 1
+        instance = {}
+        instance['info'] = 'spytensor created'
+        instance['license'] = ['license']
+        instance['images'] = self.images
+        instance['annotations'] = self.annotations
+        instance['categories'] = self.categories
+        return instance
+
+    # 构建类别
+    def _init_categories(self):
+        for k, v in classname_to_id.items():
+            category = {}
+            category['id'] = v
+            category['name'] = k
+            self.categories.append(category)
+
+    # 构建COCO的image字段
+    def _image(self, obj, path):
+        image = {}
+        from labelme import utils
+        img_x = utils.img_b64_to_arr(obj['imageData'])
+        h, w = img_x.shape[:-1]
+        image['height'] = h
+        image['width'] = w
+        image['id'] = self.img_id
+        image['file_name'] = os.path.basename(path).replace(".json", ".jpg")
+        return image
+
+    # 构建COCO的annotation字段
+    def _annotation(self, shape):
+        label = shape['label']
+        points = shape['points']
+        annotation = {}
+        annotation['id'] = self.ann_id
+        annotation['image_id'] = self.img_id
+        annotation['category_id'] = int(classname_to_id[label])
+        # annotation['segmentation'] = [np.asarray(points,dtype=np.int).flatten().tolist()]
+        annotation['segmentation'] = [list(map(int,self._get_seg(points)))]
+        annotation['bbox'] = list(map(int,self._get_box(points)))
+        annotation['iscrowd'] = 0
+        annotation['area'] = 1.0
+        return annotation
+
+    # 读取json文件，返回一个json对象
+    def read_jsonfile(self, path):
+        with open(path, "r", encoding='utf-8') as f:
+            return json.load(f)
+
+    # COCO的格式： [x1,y1,w,h] 对应COCO的bbox格式
+    def _get_box(self, points):
+        min_x = min_y = np.inf
+        max_x = max_y = 0
+        for x, y in points:
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+        return [min_x, min_y, max_x - min_x, max_y - min_y]
+
+    # Segmentation的格式：[x1,y1,x1,y2,x2,y2,x1,y2]
+    def _get_seg(self,points):
+        x1 = points[1][0] if points[0][0] > points[1][0] else points[0][0]  # min_x
+        y1 = points[1][1] if points[0][1] > points[1][1] else points[0][1]  # min_y
+        x3 = points[0][0] if points[0][0] > points[1][0] else points[1][0]  # max_x
+        y3 = points[0][1] if points[0][1] > points[1][1] else points[1][1]  # max_y
+        x2, y2 = [x3, y1]
+        x4, y4 = [x1, y3]
+        return [x1, y1, x2, y2, x3, y3, x4, y4]
+
+if __name__ == '__main__':
+    print(os.curdir)
+    labelme_path = "../DefectDataSet"
+    saved_coco_path = "../DefectDataSet"
+    print(str.format('labelme_path:{0}\nsaved_coco_path:{1}',os.path.exists(labelme_path),os.path.exists(saved_coco_path)))
+    # 创建文件
+    if not os.path.exists("%scoco/annotations/"%saved_coco_path):
+        os.makedirs("%scoco/annotations/"%saved_coco_path)
+    if not os.path.exists("%scoco/images/train2019/"%saved_coco_path):
+        os.makedirs("%scoco/images/train2019"%saved_coco_path)
+    if not os.path.exists("%scoco/images/val2019/"%saved_coco_path):
+        os.makedirs("%scoco/images/val2019"%saved_coco_path)
+    # 获取images目录下所有的joson文件列表
+    json_list_path = glob.glob(labelme_path + "/*.json")
+    # 数据划分,这里没有区分val2019和tran2019目录，所有图片都放在images目录下
+    train_path, val_path = train_test_split(json_list_path, test_size=0.12)
+    print("train_n:", len(train_path), 'val_n:', len(val_path))
+
+    # 把训练集转化为COCO的json格式
+    l2c_train = Lableme2CoCo()
+    train_instance = l2c_train.to_coco(train_path)
+    l2c_train.save_coco_json(train_instance, '%scoco/annotations/instances_train2019.json'%saved_coco_path)
+    for file in train_path:
+        shutil.copy(file.replace("json","jpg"),"%scoco/images/train2019/"%saved_coco_path)
+    for file in val_path:
+        shutil.copy(file.replace("json","jpg"),"%scoco/images/val2019/"%saved_coco_path)
+
+    # 把验证集转化为COCO的json格式
+    l2c_val = Lableme2CoCo()
+    val_instance = l2c_val.to_coco(val_path)
+    l2c_val.save_coco_json(val_instance, '%scoco/annotations/instances_val2019.json'%saved_coco_path)
+
---
+++ txt2coco.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+'''
+@time: 2020/10/28 12:06
+spytensor,zhoujianwen
+'''
+
+import os
+import json
+import numpy as np
+import pandas as pd
+import glob
+import cv2
+import os
+import shutil
+from IPython import embed
+from sklearn.model_selection import train_test_split
+
+np.random.seed(41)
+
+# 0为背景
+classname_to_id = {"__background__": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5}
+
+
+class Txt2CoCo:
+
+    def __init__(self, image_dir, total_annos):
+        self.images = []
+        self.annotations = []
+        self.categories = []
+        self.img_id = 0
+        self.ann_id = 0
+        self.image_dir = image_dir
+        self.total_annos = total_annos
+
+    def save_coco_json(self, instance, save_path):
+        json.dump(instance, open(save_path, 'w'), ensure_ascii=False, indent=2)  # indent=2 更加美观显示
+
+    # 由txt文件构建COCO
+    def to_coco(self, keys):
+        self._init_categories()
+        for key in keys:
+            self.images.append(self._image(key))
+            shapes = self.total_annos[key]
+            for shape in shapes:
+                bboxi = []
+                for cor in shape[:-1]:
+                    bboxi.append(float(cor))
+                label = shape[-1]
+                annotation = self._annotation(bboxi, label)
+                self.annotations.append(annotation)
+                self.ann_id += 1
+            self.img_id += 1
+        instance = {}
+        instance['info'] = 'spytensor created'
+        instance['license'] = ['license']
+        instance['images'] = self.images
+        instance['annotations'] = self.annotations
+        instance['categories'] = self.categories
+        return instance
+
+    # 构建类别
+    def _init_categories(self):
+        for k, v in classname_to_id.items():
+            category = {}
+            category['id'] = v
+            category['name'] = k
+            self.categories.append(category)
+
+    # 构建COCO的image字段
+    def _image(self, path):
+        image = {}
+        print(path)
+        img = cv2.imread(os.path.join(self.image_dir, path))
+        image['height'] = img.shape[0]
+        image['width'] = img.shape[1]
+        image['id'] = self.img_id
+        image['file_name'] = path
+        return image
+
+    # 构建COCO的annotation字段
+    def _annotation(self, shape, label):
+        # label = shape[-1]
+        points = shape[:8]
+        annotation = {}
+        annotation['id'] = self.ann_id
+        annotation['image_id'] = self.img_id
+        annotation['category_id'] = int(classname_to_id[label])
+        annotation['segmentation'] = [self._get_seg(points)]
+        annotation['bbox'] = self._get_box(points)
+        annotation['iscrowd'] = 0
+        annotation['area'] = 1.0
+        return annotation
+
+    # COCO的格式： [x1,y1,w,h] 对应COCO的bbox格式
+    def _get_box(self, points):  # 需要额外处理by zhoujianwen
+        min_x = min_y = np.inf
+        max_x = max_y = 0
+        arraypoints = np.array(points).reshape(4,2)
+        for x, y in arraypoints:
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+        return [min_x, min_y, max_x - min_x, max_y - min_y]
+
+    # Segmentation的格式：[x1,y1,x1,y2,x2,y2,x1,y2]
+    def _get_seg(self, points):
+        x1, y1, x2, y2, x3, y3, x4, y4 = points
+        return [x1, y1, x2, y2, x3, y3, x4, y4]
+
+
+if __name__ == '__main__':
+    txt_file = ".\\data\\train\\labels"
+    image_dir = ".\\data\\train\\images"
+    saved_coco_path = "outputs"
+    # 整合txt格式标注文件
+    total_txt_annotations = {}
+    annotations = []
+
+    files = os.listdir(txt_file)
+    files.sort()
+
+    # 获取images目录下所有的txt文件列表
+    txt_list_path = glob.glob(os.path.join(txt_file,"*.txt"))
+    for f in txt_list_path:
+        fh1 = open(f, "r", encoding="UTF-8")
+        nameOfImage = f.replace("txt", "tif")
+        for line in fh1:
+            line = line.replace("\n", "")
+            if line.replace(' ', '') == '':
+                continue
+            splitLine = line.split(" ")
+
+            cls = (splitLine[0])  # class
+            x1 = float(splitLine[1])
+            y1 = float(splitLine[2])
+            x2 = float(splitLine[3])
+            y2 = float(splitLine[4])
+            x3 = float(splitLine[5])
+            y3 = float(splitLine[6])
+            x4 = float(splitLine[7])
+            y4 = float(splitLine[8])
+            one_box = [nameOfImage, x1, y1, x2, y2, x3, y3, x4, y4, cls]
+            annotations.append(one_box)
+        fh1.close()
+
+    # annotations = pd.read_csv(csv_file, header=None).values
+    for annotation in annotations:
+        key = annotation[0].split(os.sep)[-1]
+        value = np.array([annotation[1:]])
+        if key in total_txt_annotations.keys():
+            total_txt_annotations[key] = np.concatenate((total_txt_annotations[key], value), axis=0)
+        else:
+            total_txt_annotations[key] = value
+    # 按照键值划分数据
+    total_keys = list(total_txt_annotations.keys())
+
+    train_keys, test_keys = train_test_split(total_keys, test_size=0.2)
+    #train_keys = total_keys
+    #test_keys = []
+    print("train_n:", len(train_keys), 'test_n:', len(test_keys))
+    # 创建必须的文件夹
+    if not os.path.exists('%scoco/annotations/' % saved_coco_path):
+        os.makedirs('%scoco/annotations/' % saved_coco_path)
+    if not os.path.exists('%scoco/images/train2020/' % saved_coco_path):
+        os.makedirs('%scoco/images/train2020/' % saved_coco_path)
+    if not os.path.exists('%scoco/images/test2020/' % saved_coco_path):
+        os.makedirs('%scoco/images/test2020/' % saved_coco_path)
+    # 把训练集转化为COCO的json格式
+    l2c_train = Txt2CoCo(image_dir=image_dir, total_annos=total_txt_annotations)
+    train_instance = l2c_train.to_coco(train_keys)
+    l2c_train.save_coco_json(train_instance, '%scoco/annotations/instances_train2020.json' % saved_coco_path)
+    for file in train_keys:
+        shutil.copy(os.path.join(image_dir,file), "%scoco/images/train2020/" % saved_coco_path)
+    for file in test_keys:
+        shutil.copy(os.path.join(image_dir,file), "%scoco/images/test2020/" % saved_coco_path)
+    # 把验证集转化为COCO的json格式
+    l2c_val = Txt2CoCo(image_dir=image_dir, total_annos=total_txt_annotations)
+    val_instance = l2c_val.to_coco(test_keys)
+    l2c_val.save_coco_json(val_instance, '%scoco/annotations/instances_test2020.json' % saved_coco_path)
